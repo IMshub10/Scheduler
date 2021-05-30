7b8f040d89f8edc196c750a7b716c03c95836cb3
Vaasuawesome7 <55234743+Vaasuawesome7@users.noreply.github.com>
2021-05-30 21:21:58 +0530

    Added Dao for reminder and to do

---
+++ app/src/main/java/com/summer/scheduler/data/model/dao/ReminderDao.kt
@@ -0,0 +1,24 @@
+package com.summer.scheduler.data.model.dao
+
+import androidx.room.*
+import com.summer.scheduler.data.model.entity.ReminderEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ReminderDao {
+
+    @Insert
+    suspend fun addReminder(reminder: ReminderEntity)
+
+    @Update
+    suspend fun updateReminder(reminder: ReminderEntity)
+
+    @Delete
+    suspend fun deleteReminder(reminder: ReminderEntity)
+
+    @Query("DELETE FROM reminder_table")
+    suspend fun deleteAllReminders()
+
+    @Query("SELECT * FROM reminder_table ORDER BY `key` DESC")
+    suspend fun getAllReminders(): Flow<ArrayList<ReminderEntity>>
+}
