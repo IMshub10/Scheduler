ce227eea21d8fef22270e313c297356c16cf856b
Vaasuawesome7 <55234743+Vaasuawesome7@users.noreply.github.com>
2021-05-30 18:09:39 +0530

    Changed the adapters

--- app/src/main/java/com/summer/scheduler/ui/main/adapter/ToDoListAdapter.kt
+++ app/src/main/java/com/summer/scheduler/ui/main/adapter/ToDoListAdapter.kt
@@ -1,15 +1,23 @@
 package com.summer.scheduler.ui.main.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.summer.scheduler.R
-import com.summer.scheduler.data.model.recycler_view_items.ToDoItem
+import com.summer.scheduler.data.model.entity.ToDoEntity
 
-class ToDoListAdapter(private val toDoItems: ArrayList<ToDoItem>): RecyclerView.Adapter<ToDoListAdapter.ToDoItemHolder>() {
+class ToDoListAdapter(private val context: Context) :
+    ListAdapter<ToDoEntity, ToDoListAdapter.ToDoItemHolder>(DIFF_CALLBACK) {
+    class ToDoItemHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val checkBox: CheckBox = itemView.findViewById(R.id.checkBox_toDoCheck)
+        val title: TextView = itemView.findViewById(R.id.textView_toDoText)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoItemHolder {
         val view = LayoutInflater.from(parent.context)
@@ -18,22 +26,23 @@ class ToDoListAdapter(private val toDoItems: ArrayList<ToDoItem>): RecyclerView.
     }
 
     override fun onBindViewHolder(holder: ToDoItemHolder, position: Int) {
-        holder.checkBox.isChecked = toDoItems[position].check
-        holder.title.text = toDoItems[position].toDo
+        val toDo = getItem(position)
+        holder.checkBox.isChecked = toDo.check
+        holder.title.text = toDo.title
     }
 
-    override fun getItemCount(): Int {
-       return toDoItems.size
+    companion object {
+        val DIFF_CALLBACK: DiffUtil.ItemCallback<ToDoEntity> =
+            object : DiffUtil.ItemCallback<ToDoEntity>() {
+
+                override fun areItemsTheSame(oldItem: ToDoEntity, newItem: ToDoEntity): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+                override fun areContentsTheSame(oldItem: ToDoEntity, newItem: ToDoEntity): Boolean {
+                    TODO("Not yet implemented")
+                }
+            }
     }
 
-    class ToDoItemHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val checkBox: CheckBox = itemView.findViewById(R.id.checkBox_toDoCheck)
-        val title: TextView = itemView.findViewById(R.id.textView_toDoText)
-    }
-
-    fun addItems(items: ArrayList<ToDoItem>) {
-        toDoItems.clear()
-        toDoItems.addAll(items)
-        notifyDataSetChanged()
-    }
 }
