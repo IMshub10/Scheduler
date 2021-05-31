f668bac6a468e8e75c6e3f2db6b0e21ebdbb4b1f
Aditi-Gautam <aditi.gautam1924@gmail.com>
2021-05-31 15:39:44 +0530

    Refactor

--- app/src/main/java/com/summer/scheduler/ui/main/view/NewToDoBottomSheetFragment.kt
+++ app/src/main/java/com/summer/scheduler/ui/main/view/NewToDoBottomSheetFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.summer.scheduler.R
+import kotlinx.android.synthetic.main.fragment_reminders.view.*
+import kotlinx.android.synthetic.main.schedule_main.view.*
 
 class NewToDoBottomSheetFragment : BottomSheetDialogFragment() {
 
@@ -18,6 +20,16 @@ class NewToDoBottomSheetFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_reminders, container, false)
+        val view = inflater.inflate(R.layout.fragment_reminders, container, false)
+
+        view.to_do_done.setOnClickListener {
+            insertToDotoDatabase()
+        }
+
+        return view
+    }
+
+    private fun insertToDotoDatabase() {
+
     }
 }
