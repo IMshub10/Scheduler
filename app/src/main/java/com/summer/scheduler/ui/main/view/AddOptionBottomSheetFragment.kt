178d1621aeee7d7007853404e3eb27b97d76e8a9
Aditi-Gautam <aditi.gautam1924@gmail.com>
2021-06-01 12:18:36 +0530

    Navigation dependencies

--- app/src/main/java/com/summer/scheduler/ui/main/view/AddOptionBottomSheetFragment.kt
+++ app/src/main/java/com/summer/scheduler/ui/main/view/AddOptionBottomSheetFragment.kt
@@ -2,10 +2,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ToggleButton
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.summer.scheduler.R
+import com.summer.scheduler.ui.main.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.events_and_reminders.*
 
 class AddOptionBottomSheetFragment : BottomSheetDialogFragment() {
+
+    private lateinit var toggle: ToggleButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,16 @@ class AddOptionBottomSheetFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.events_and_reminders, container, false)
+        val view = inflater.inflate(R.layout.events_and_reminders, container, false)
+
+        materialButton_toDo.isSelected = true
+        materialButton_toDo.setOnClickListener {
+
+        }
+        materialButton_today.setOnClickListener {
+
+        }
+
+        return view;
     }
 }
