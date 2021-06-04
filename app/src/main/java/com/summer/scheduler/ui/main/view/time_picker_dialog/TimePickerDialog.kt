eda9e9abb62c0c1adb4b104149421c26ef2390cd
Vaasuawesome7 <55234743+Vaasuawesome7@users.noreply.github.com>
2021-06-04 18:13:26 +0530

    Integrated to do dialog in reminder using LiveData

--- app/src/main/java/com/summer/scheduler/ui/main/view/time_picker_dialog/TimePickerDialog.kt
+++ app/src/main/java/com/summer/scheduler/ui/main/view/time_picker_dialog/TimePickerDialog.kt
@@ -2,11 +2,17 @@ package com.summer.scheduler.ui.main.view.time_picker_dialog
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
 import com.summer.scheduler.R
-import kotlinx.android.synthetic.main.component_time_picker.*
+import com.summer.scheduler.ui.main.viewmodel.MainViewModel
+import com.summer.scheduler.ui.main.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.component_time_picker.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,10 +25,13 @@ class TimePickerDialog: AppCompatDialogFragment() {
         val time = Calendar.getInstance().time
         val format = SimpleDateFormat("HHmm", Locale.getDefault())
         val timeString = format.format(time)
-        selectedHour = timeString.substring(0..2).toInt()
-        selectedMin = timeString.substring(2..4).toInt()
+        selectedHour = timeString.substring(0, 2).toInt()
+        selectedMin = timeString.substring(2, 4).toInt()
         Log.e("TimePickerDialog", "timeString: $timeString")
     }
+
+    private lateinit var doneButton: Button
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(
@@ -30,17 +39,47 @@ class TimePickerDialog: AppCompatDialogFragment() {
         )
         val inflater = requireActivity().layoutInflater
         val view = inflater.inflate(R.layout.component_time_picker, null)
-        val timePicker = timePickerDialog
+        val timePicker = view.timePickerDialog
+
+        mainViewModel = ViewModelProvider(
+            requireActivity(),
+            ViewModelFactory(requireActivity().application)
+        ).get(MainViewModel::class.java)
+
+        doneButton = view.button_timePickerSet
+
+        doneButton.setOnClickListener {
+            if (mStart) {
+                Log.e("doneButtonOnClick", "Here start")
+                mainViewModel.startTimeArray.value = getTime()
+            }
+            else {
+                Log.e("doneButtonOnClick", "Here stop")
+                mainViewModel.stopTimeArray.value = getTime()
+            }
+            dismiss()
+        }
 
         timePicker.setOnTimeChangedListener { _, i, i2 ->
             selectedHour = i
             selectedMin = i2
+            Log.e("timePicker.setOnTimeChangedListener", "$selectedHour $selectedMin")
         }
         builder.setView(view)
         return builder.create()
     }
 
-    fun getTime(): Array<Int> {
+    private fun getTime(): Array<Int> {
         return arrayOf(selectedHour, selectedMin)
+    }
+
+    companion object {
+        private var mStart = true
+        fun newInstance(bundle: Bundle, start: Boolean): TimePickerDialog {
+            val fragment = TimePickerDialog()
+            fragment.arguments = bundle
+            mStart = start
+            return fragment
+        }
     }
 }
